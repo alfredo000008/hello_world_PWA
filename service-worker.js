4055d80e4cf4865063f0816ae6aece170ce1505a
alfredo000008 <alfredogasa8@hotmail.com>
2024-04-02 13:52:27 -0600

    sw js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,17 @@
 // service-worker.js
 
-// Instalar el service worker
+// Service Worker installation
 self.addEventListener('install', event => {
-    console.log('Service Worker instalado');
-  });
-  
-  // Activar el service worker
-  self.addEventListener('activate', event => {
-    console.log('Service Worker activado');
-  });
-  
-  // Interceptar y responder a las solicitudes de red
-  self.addEventListener('fetch', event => {
-    console.log('Interceptando solicitud:', event.request.url);
-    
-    // Aquí puedes implementar lógica para responder a las solicitudes de red, como el cacheado de recursos
-  });
-  
+  console.log('Service Worker installed');
+});
+
+// Service Worker activation
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated');
+});
+
+// Intercept and respond to fetch requests
+self.addEventListener('fetch', event => {
+  console.log('Intercepting request:', event.request.url);
+  // Additional fetch handling logic here
+});
